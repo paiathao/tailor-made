ca71444f150b2c9e9b34d689e9797514701c8596
Paia Thao <paiathao09@gmail.com>
2018-08-15 13:38:29 -0500

    add multiple selects

--- src/components/ServiceSelector/ServiceSelector.jsx
+++ src/components/ServiceSelector/ServiceSelector.jsx
@@ -14,7 +14,7 @@ class ServiceSelector extends React.Component {
 
     this.state = {
       selected: [],
-      service: []
+      rowIndex: [],
     }
   }
 
@@ -22,75 +22,83 @@ class ServiceSelector extends React.Component {
 
     const data = this.props.serviceList
 
-    console.log('state of selected', this.state.selected)
-    console.log('state of service', this.state.service)
-
     return (
-        <div>
-           <ReactTable 
-           columns={[
+      <div>
+        <ReactTable
+          columns={[
             {
               Header: "Category",
               accessor: "category",
               filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, { keys: ["category"] }),
-            filterAll: true
+                matchSorter(rows, filter.value, { keys: ["category"] }),
+              filterAll: true
             },
             {
               Header: "Service",
               accessor: "service",
               filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, { keys: ["service"] }),
-            filterAll: true
+                matchSorter(rows, filter.value, { keys: ["service"] }),
+              filterAll: true
             },
             {
               Header: "Cost",
               accessor: "cost",
               filterMethod: (filter, rows) =>
-              matchSorter(rows, filter.value, { keys: ["cost"] }),
-            filterAll: true
+                matchSorter(rows, filter.value, { keys: ["cost"] }),
+              filterAll: true
             },
           ]}
-           data={data}
-           filterable
-           defaultPageSize={15}
-           className="-striped -highlight"
-           getTrGroupProps={(state, rowInfo, column, instance) => {
+          data={data}
+          filterable
+          defaultPageSize={15}
+          className="-striped -highlight"
+          getTrGroupProps={(state, rowInfo, column, instance) => {
             if (rowInfo !== undefined) {
-                return {
-                    onClick: (e, handleOriginal) => {
-                      console.log('It was in this row:', rowInfo.row)
-                      this.setState({
-                        service : {
-                          category: rowInfo.row.category,
-                          service: rowInfo.row.service,
-                          cost: rowInfo.row.cost,
-                          id: rowInfo.original._id
-                        },
-                        selected : {
-                          ...this.state.selected,
-                          selectedIndex: rowInfo.original._id
-                        }  
-                      })
-                    },
-                    style: {
-                        cursor: 'pointer',
-                        background: rowInfo.original._id === this.state.selected.selectedIndex ? '#00afec' : 'white',
-                        color: rowInfo.original._id === this.state.selected.selectedIndex  ? 'white' : 'black'                      
-                    }
+              return {
+                onClick: (e, handleOriginal) => {
+
+                  let a = this.state.selected.indexOf(rowInfo.row);
+
+                  if (a == -1) {
+                    this.setState({
+                      selected: [
+                        ...this.state.selected, {
+                        category: rowInfo.row.category,
+                        service: rowInfo.row.service,
+                        cost: rowInfo.row.cost,
+                        id: rowInfo.original._id
+                      }],
+                      rowIndex: [...this.state.rowIndex, rowInfo.index],
+                    
+                    })
+                  }
+
+                  let array = this.state.selected;
+
+                  if (a != -1) {
+                    array.splice(a, 1);
+                    this.setState({ selected: array });
+                  }
+                },
+                style: {
+                  cursor: 'pointer',
+                  background: this.state.rowIndex.indexOf(rowInfo.index) != -1 ? '#00afec' : 'white',
+                  color: this.state.rowIndex.indexOf(rowInfo.index) != -1 ? 'white' : 'black'
                 }
-            }}
-        }
-           />
+              }
+            }
+          }
+          }
+        />
       </div>
-  );
-}
+    );
+  }
 }
 
 
 
 const mapStateToProps = (state) => {
-    return { serviceList: state.serviceList }
+  return { serviceList: state.serviceList }
 }
 
 
